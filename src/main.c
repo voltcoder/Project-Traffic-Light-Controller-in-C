4fe17c5fa43c7fcfc48d50e4d14dce53bb3ef1b3
Swati Sharma <swatisharma83915@gmail.com>
2026-02-23 16:09:44 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,26 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
 
+int main(void)
+{
+    // Set PD0, PD1, PD2 as output
+    DDRD |= (1 << PD0) | (1 << PD1) | (1 << PD2);
+
+    while (1)
+    {
+        // RED ON
+        PORTD = (1 << PD0);
+        _delay_ms(5000);
+
+        // YELLOW ON
+        PORTD = (1 << PD1);
+        _delay_ms(2000);
+
+        // GREEN ON
+        PORTD = (1 << PD2);
+        _delay_ms(5000);
+    }
+
+    return 0;
+}
